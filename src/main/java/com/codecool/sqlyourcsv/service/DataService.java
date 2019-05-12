d1cb1b3643fbc2ee83f4123b6843d81f92247043
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-05-12 21:26:58 +0200

    Create dataLoader field and inject it

--- src/main/java/com/codecool/sqlyourcsv/service/DataService.java
+++ src/main/java/com/codecool/sqlyourcsv/service/DataService.java
@@ -1,4 +1,16 @@
 package com.codecool.sqlyourcsv.service;
 
+import com.codecool.sqlyourcsv.utils.DataLoader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class DataService {
+
+    private DataLoader dataLoader;
+
+    @Autowired
+    public DataService(DataLoader dataLoader) {
+        this.dataLoader = dataLoader;
+    }
 }
