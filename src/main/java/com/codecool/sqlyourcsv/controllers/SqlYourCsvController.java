6035a3ae7be30f011ca59c38c0603bcdc93bfa2f
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-05-12 13:06:51 +0200

    Add Model attribute to hello method Create UserQuery object Implement result method

--- src/main/java/com/codecool/sqlyourcsv/controllers/SqlYourCsvController.java
+++ src/main/java/com/codecool/sqlyourcsv/controllers/SqlYourCsvController.java
@@ -1,13 +1,23 @@
 package com.codecool.sqlyourcsv.controllers;
 
+import com.codecool.sqlyourcsv.model.UserQuery;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SqlYourCsvController {
 
     @GetMapping("/sql-your-csv")
-    public String hello() {
+    public String hello(Model model) {
+        model.addAttribute("query", new UserQuery());
         return "sql_your_csv";
+    }
+
+    @PostMapping("/sql-your-csv")
+    public String result(@ModelAttribute UserQuery userQuery) {
+        return "result";
     }
 }
