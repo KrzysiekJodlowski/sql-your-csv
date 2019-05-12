d1254f8188fa9e3687cc9ea7293453ec8415f9b8
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-05-12 13:09:08 +0200

    Implement UserQuery class

--- src/main/java/com/codecool/sqlyourcsv/model/UserQuery.java
+++ src/main/java/com/codecool/sqlyourcsv/model/UserQuery.java
@@ -1,4 +1,14 @@
 package com.codecool.sqlyourcsv.model;
 
 public class UserQuery {
+
+    private String content;
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
